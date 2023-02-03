5b131e21d6e947acf4da91f87a7bf42e9be64fe3
npv34 <phanvanluan611996@gmail.com>
2023-02-03 15:16:58 +0700

    demo session cookie

--- handles/Handle.js
+++ handles/Handle.js
@@ -1,6 +1,8 @@
 const BaseHandle = require("./BaseHandle");
 const url = require("url");
 const qs = require("qs");
+const fs = require("fs");
+const cookie = require('cookie');
 
 class Handle extends BaseHandle{
     async showDashboard(req, res) {
@@ -24,8 +26,10 @@ class Handle extends BaseHandle{
             newHTML += `<td>${user.email}</td>`;
             newHTML += `<td>${(user.role == 1) ? 'admin' : 'user'}</td>`;
             newHTML += `<td>${user.phone}</td>`;
-            newHTML += `<td><a onclick="return confirm('Are you sure you want to delete this user?')" href="/admin/users/delete?id=${user.id}" class="btn btn-danger">Delete</a></td>`;
-
+            newHTML += `<td>
+                            <a onclick="return confirm('Are you sure you want to delete this user?')" href="/admin/users/delete?id=${user.id}" class="btn btn-danger">Delete</a>
+                            <a href="/admin/users/update?id=${user.id}" class="btn btn-primary">Update</a>
+                        </td>`;
             newHTML += '</tr>';
         });
         // lay data sql thay doi html
@@ -63,8 +67,87 @@ class Handle extends BaseHandle{
             res.writeHead(301, {Location: '/admin/users'});
             res.end();
         })
-
     }
+
+    async showFormUpdateUser(req, res) {
+        let html = await this.getTemplate('./src/view/users/update.html');
+        // thuc hien truy van
+        let query = url.parse(req.url).query;
+        let id = qs.parse(query).id;
+        let sql = 'SELECT * FROM users WHERE id = ' + id;
+        let data = await this.querySQL(sql);
+        html = html.replace('{name}', data[0].name)
+        html = html.replace('{username}', data[0].username)
+        html = html.replace('{email}', data[0].email)
+        html = html.replace('{address}', data[0].address)
+        html = html.replace('{phone}', data[0].phone)
+        html = html.replace('{id}', data[0].id)
+
+        let  roleHTML = `
+            <option ${(data[0].role == 1) ? 'selected': ''} value="1">Admin</option>
+            <option ${(data[0].role == 2) ? 'selected' : ''} value="2">User</option>
+        `;
+
+        html = html.replace('{role}', roleHTML)
+        res.write(html)
+        res.end();
+    }
+
+    async updateUser(req, res) {
+        let query = url.parse(req.url).query;
+        let id = qs.parse(query).id;
+        // lay du  lieu tu  form
+        let data = '';
+        req.on('data', chunk => {
+            data += chunk
+        })
+        req.on('end', async () => {
+            let dataForm = qs.parse(data);
+            let sql = `CALL updateUser('${dataForm.name}','${dataForm.role}','${dataForm.address}', '${dataForm.phone}', '${id}')`
+            await this.querySQL(sql);
+            res.writeHead(301, {Location: '/admin/users'});
+            res.end();
+        })
+    }
+
+    async showFormLogin(req, res) {
+        let html = await this.getTemplate('./src/view/login.html');
+        res.write(html)
+        res.end();
+    }
+
+    async login(req, res) {
+        let data = '';
+        req.on('data', chunk => {
+            data += chunk
+        })
+        req.on('end', async () => {
+            let dataForm = qs.parse(data);
+            let sql = `SELECT name, username, email, phone, role FROM users WHERE username = '${dataForm.username}' AND password = '${dataForm.password}'`;
+            let result = await this.querySQL(sql);
+            if (result.length == 0) {
+                res.writeHead(301, {Location: '/admin/login'})
+                return res.end();
+            } else {
+                // tao session luu thong tin dang nhap
+                // tao ten file session
+                let nameFileSessions = result[0].username + '.txt';
+                let dataSession = JSON.stringify(result[0]);
+
+                await this.writeFile('./sessions/' + nameFileSessions, dataSession)
+
+                // tao cookie
+                // gan cookie vao header res
+                res.setHeader('Set-Cookie','u_user=' + result[0].username);
+
+                res.writeHead(301, {Location: '/admin/users'});
+                return res.end()
+
+            }
+
+        })
+    }
+
 }
 
 module.exports = new Handle();
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const url = require('url');
 const Handler = require('./handles/Handle')
+const qs = require("qs");
 
 const server = http.createServer((req, res) => {
     const pathName = url.parse(req.url).pathname;
@@ -14,6 +15,16 @@ const server = http.createServer((req, res) => {
             });
             break;
         case '/admin/users':
+            // kiem tra session
+            // lay  thong tin session tu cookie cua request
+            let cookie = req.headers.cookie;
+
+            let usernameLogin = qs.parse(cookie).u_user;
+            if (!usernameLogin) {
+                res.writeHead(301, {Location: '/admin/login'})
+                return res.end();
+            }
+
             Handler.showListUsers(req, res).catch(err => {
                 console.log(err.message)
             });
@@ -24,6 +35,7 @@ const server = http.createServer((req, res) => {
             })
             break;
         case '/admin/users/create':
+
             Handler.showFormCreateUser(req, res).catch(err => {
                 console.log(err.message)
             })
@@ -33,6 +45,28 @@ const server = http.createServer((req, res) => {
                 console.log(err.message)
             })
             break;
+        case '/admin/users/update':
+            Handler.showFormUpdateUser(req, res).catch(err => {
+                console.log(err.message)
+            })
+            break;
+        case '/admin/users/edit':
+            Handler.updateUser(req, res).catch(err => {
+                console.log(err.message)
+            })
+            break
+        case '/admin/login':
+            if (methodRequest == 'GET') {
+                Handler.showFormLogin(req, res).catch(err => {
+                    console.log(err.message)
+                })
+            } else {
+                Handler.login(req, res).catch(err => {
+                    console.log(err.message)
+                })
+            }
+            break
+
         default:
             res.end();
     }
